98ec3ad5bef0638a77cf83b5e7cd8489da191a2d
Angelina <pankova1797@gmail.com>
2025-02-16 03:26:15 +0300

    Delete Character/character-class directory

--- Character/character-class/src/index.js
+++
@@ -1,9 +0,0 @@
-const Character = require('./Character');
-const Bowerman = require('./Bowerman');
-const Swordsman = require('./Swordsman');
-const Magician = require('./Magician');
-const Daemon = require('./Daemon');
-const Undead = require('./Undead');
-const Zombie = require('./Zombie');
-
-module.exports = { Character, Bowerman, Swordsman, Magician, Daemon, Undead, Zombie };
